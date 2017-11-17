5c3d7929e5ae16e7ca9422a1b57fb1d851ed4ccd
Valentin Dide <vali.dide@gmail.com>
2017-11-17 23:12:02 +0200

    Created NuGet package
    Created NuGet package
    
    https://www.nuget.org/packages/MobileDetect/0.0.1

--- _build/_utilities.ps1
+++ _build/_utilities.ps1
@@ -2,27 +2,6 @@
 # A set o utilities used to build the project
 #
 
-function Patch-File([string] $filePath, [string] $match, [string] $replace) {
-	(Get-Content $filePath) -replace $match, $replace | Out-File $filePath
-}
-
-function Patch-Nuspec-Version([string] $filePath, [string] $version) {
-	$path = $filePath
-	$match = '<version>.*<\/version>'
-	$replace = "<version>$version</version>"
-	
-	Patch-File $path $match $replace
-}
-
-function Patch-ProjectJson-Version([string] $filePath, [string] $version) {
-	$path = $filePath
-	$match = '"version": ".*"'
-	$replace = '"version": "' + $version + '"'
-	
-	Patch-File $path $match $replace
-}
-
-
 function Resolve-HeaderName-FromPhp([string] $phpHeader) {
     $header = $phpHeader.ToLowerInvariant();
     if ($header.StartsWith('http_')) {

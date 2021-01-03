b6efe1b642108ab0b9450cb4b542f3a83e9158af
Valentin Dide <vali.dide@gmail.com>
2021-01-03 21:56:06 +0200

    CI Fix

--- _build/test.ps1
+++ _build/test.ps1
@@ -1,11 +1,25 @@
+param(
+  [Parameter(Mandatory = $false)]
+  [string] $Rebuild = 'true',
+  [Parameter(Mandatory = $false)]
+  [string] $Configuration = 'Debug'
+)
+
 $coveragePath = [System.IO.Path]::GetFullPath("$($PSScriptRoot)/../coverage/")
 $params = New-Object System.Collections.ArrayList
-[void] $params.Add('--no-build')
-[void] $params.Add('/p:CollectCoverage=true')
-[void] $params.Add('/p:CoverletOutputFormat=\`"lcov,opencover\`"')
-[void] $params.Add("/p:CoverletOutput='$coveragePath'")
-[void] $params.Add('/p:Threshold=95')
-[void] $params.Add('/p:UseSourceLink=true')
+
+if ($Rebuild -inotmatch 'true') {
+  [void] $params.Add('--no-build')
+}
+
+if ($Configuration -imatch 'Debug') { # Check coverage only on Debug BUILD (this will be non deterministic)
+  [void] $params.Add('/p:CollectCoverage=true')
+  [void] $params.Add('/p:CoverletOutputFormat=\`"lcov,opencover\`"')
+  [void] $params.Add("/p:CoverletOutput='$coveragePath'")
+  # [void] $params.Add('/p:Include="[MobileDetect.*.]*"')
+  [void] $params.Add('/p:Threshold=95')
+  [void] $params.Add('/p:UseSourceLink=true')
+}
 
 if (Test-Path -Path $coveragePath -PathType Container) {
   Remove-Item -Path $coveragePath -Recurse -Force
@@ -13,7 +27,7 @@ if (Test-Path -Path $coveragePath -PathType Container) {
 
 $expresionParams = $params -join ' '
 # Write-Host $expresionParams
-Invoke-Expression "dotnet test $expresionParams"
+Invoke-Expression "dotnet test --configuration $Configuration $expresionParams"
 if ($LASTEXITCODE -ne 0) {
   exit $LASTEXITCODE
 }

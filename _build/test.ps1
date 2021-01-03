ca6c72c1f270f2966f8bf2bcee2486a9af3dfe2d
Valentin Dide <vali.dide@gmail.com>
2021-01-03 22:23:15 +0200

    CI

--- _build/test.ps1
+++ _build/test.ps1
@@ -2,7 +2,9 @@ param(
   [Parameter(Mandatory = $false)]
   [string] $Rebuild = 'true',
   [Parameter(Mandatory = $false)]
-  [string] $Configuration = 'Debug'
+  [string] $Configuration = 'Debug',
+  [Parameter(Mandatory = $false)]
+  [string] $SourceLink = 'true'
 )
 
 $coveragePath = [System.IO.Path]::GetFullPath("$($PSScriptRoot)/../coverage/")
@@ -18,7 +20,9 @@ if ($Configuration -imatch 'Debug') { # Check coverage only on Debug BUILD (this
   [void] $params.Add("/p:CoverletOutput='$coveragePath'")
   # [void] $params.Add('/p:Include="[MobileDetect.*.]*"')
   [void] $params.Add('/p:Threshold=95')
-  [void] $params.Add('/p:UseSourceLink=true')
+  if ($Rebuild -imatch 'true') {
+    [void] $params.Add('/p:UseSourceLink=true')
+  }
 }
 
 if (Test-Path -Path $coveragePath -PathType Container) {
